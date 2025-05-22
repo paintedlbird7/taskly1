6f8a8c9e22c5f532ce6649fa760009459685c867
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-05-22 16:54:11 -0700

    Use a ScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,72 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View, ScrollView } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState(initialList);
+  const [value, setValue] = useState<string>();
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue(undefined);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Milk" isCompleted />
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+    >
+      <TextInput
+        value={value}
+        style={styles.textInput}
+        onChangeText={setValue}
+        placeholder="E.g Coffee"
+        onSubmitEditing={handleSubmit}
+        returnKeyType="done"
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem name={item.name} key={item.id} />
+      ))}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    flex: 1,
+    paddingTop: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    backgroundColor: theme.colorWhite,
   },
 });
